419bec8733b1605fa34de242a2e0c6e2636c0fc5
Aljamaan-97 <aljm3an.94@gmail.com>
2025-05-28 20:41:35 +0300

    use qurry

--- app/(protected)/(tabs)/(home)/index.tsx
+++ app/(protected)/(tabs)/(home)/index.tsx
@@ -1,6 +1,28 @@
 import ProfileCard from "@/components/ProfileCard";
 import { View } from "react-native";
+
 const Index = () => {
+  // const { data, error, isLoading } = useQuery({
+  //   queryKey: ["profile"],
+  //   queryFn: getMyProfile,
+  // });
+
+  // if (isLoading) {
+  //   return (
+  //     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+  //       <Text>Loading...</Text>
+  //     </View>
+  //   );
+  // }
+
+  // if (error) {
+  //   return (
+  //     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+  //       <Text>Error loading profile</Text>
+  //     </View>
+  //   );
+  // }
+
   return (
     <View>
       <View
